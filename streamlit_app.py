8f448a73789d03d909b6c9b165b68df16b3ddbcc
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2024-05-27 15:18:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def icon(emoji: str):
     st.write(f'<span style="font-size: 78px; line-height: 1">{emoji}</span>', unsafe_allow_html=True)
 
 icon("👽")
-st.markdown('<a href="https://groqhealth.streamlit.app/" style="text-decoration:none; color: #ADD8E6;"><h2>Vers3Dynamics</h2></a>', unsafe_allow_html=True)
+st.markdown('<a href="https://gist.githubusercontent.com/topherchris420/a8b24f8790d1f98a515138cca59a69b8/raw/535b4bbef00c769ea09b70b89941a2e6c12a0e22/Hello/" style="text-decoration:none; color: #ADD8E6;"><h2>Vers3Dynamics</h2></a>', unsafe_allow_html=True)
 st.subheader("Say Hello to Bashar, Powered by Groq 🚀")
 
 # Add a picture with a caption
