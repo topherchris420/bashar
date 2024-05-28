166162b37372db31a9c648855fe7479dd25133ed
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2024-05-28 06:49:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     model_option = st.selectbox(
-        "Connect with the perfect AI:",
+        "Greetings to you",
         options=list(models.keys()),
         format_func=lambda x: models[x]["name"],
         index=2  # Default to LLaMA
