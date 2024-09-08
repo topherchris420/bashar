1421260cfb6bce76d27a233bc71aef48b0a13e53
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2024-09-08 12:36:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ models = {
     "gemma-7b-it": {"name": "Gemma-7b-it", "tokens": 8192, "developer": "Google"},
     "llama2-70b-4096": {"name": "LLaMA2-70b-chat", "tokens": 4096, "developer": "Meta"},
     "llama3-70b-8192": {"name": "LLaMA3-70b-8192", "tokens": 8192, "developer": "Meta"},
-    "llama-3.1-70b-versatile": {"name": "LLaMA-3.1-70b-Versatile", "tokens": 8192, "developer": "Meta"},
+    "llama3-3.1-8b-instant": {"name": "LLaMA3-3.1-8b-instant", "tokens": 8192, "developer": "Meta"},
     "llama3-8b-8192": {"name": "LLaMA3-8b-8192", "tokens": 8192, "developer": "Meta"},
     "mixtral-8x7b-32768": {"name": "Mixtral-8x7b-Instruct-v0.1", "tokens": 32768, "developer": "Mistral"},
 }
